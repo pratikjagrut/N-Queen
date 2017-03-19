b6a62d40114d5ce8b34588af8d0a91dfe4076831
pratik jagrut <pratik.jagrut@gmail.com>
2017-03-19 21:12:18 +0530

    Updated code

--- app/src/main/java/com/pokemon/ash/n_queen/Main3Activity.java
+++ app/src/main/java/com/pokemon/ash/n_queen/Main3Activity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Main3Activity extends AppCompatActivity {
     public EditText e;
@@ -17,8 +18,16 @@ public class Main3Activity extends AppCompatActivity {
 
     public void solution(View view) {
         String s = e.getText().toString();
-        Intent i = new Intent(Main3Activity.this, SolutionActivity.class);
-        i.putExtra("dimension", s);
-        startActivity(i);
+        if(!s.isEmpty()) {
+            int N = Integer.parseInt(s);
+            if(N >= 4 && N <= 11){
+                Intent i = new Intent(Main3Activity.this, SolutionActivity.class);
+                i.putExtra("dimension", s);
+                startActivity(i);
+            }
+            else
+                Toast.makeText(Main3Activity.this, "Range is from 4-11", Toast.LENGTH_SHORT).show();
+        }else
+            Toast.makeText(Main3Activity.this, "Enter valid number", Toast.LENGTH_SHORT).show();
     }
 }
